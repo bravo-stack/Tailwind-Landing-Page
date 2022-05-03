acbce166d0481c4471df5025dc05ff946e4a16c7
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-03 14:18:06 +0100

    created a backdrop component that appears over the App's body when the navbar is open

---
+++ src/components/Backdrop.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Backdrop() {
+  return (
+    <div style={{
+        postition: "relative",
+        width: "100vw",
+        height: "100vh",
+    }}>
+        <div style={{
+            width: "100%",
+            height: "100%",
+            postion: "absolute",
+            backgroundColor: "rgba(0,0,0,.5)"
+        }}>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Backdrop
