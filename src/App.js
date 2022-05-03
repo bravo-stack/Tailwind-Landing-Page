c626780188a29307cad28c232d08b917ae806499
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-03 14:18:29 +0100

    imported needed components and made a few fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import Backdrop from "./components/Backdrop";
 import Header from "./components/Header";
+import Showcase from "./components/Showcase";
 
 function App() {
+  const [navBarOpen, setNavBarOpen] = useState(false)
+
+  // toggle navBar 
+  const toggleNav = () => {
+    setNavBarOpen(!navBarOpen)
+  }
+
+
   return (
-    <div>
-      <Header />
+    <div className>
+      <Header navBarOpen={navBarOpen} toggleNav={toggleNav} />
+      {navBarOpen&&<Backdrop />}
+      <Showcase />
     </div>
   );
 }
