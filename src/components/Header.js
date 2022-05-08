2994f48ae32fbbb1b9c774cdf9a549f3353dc9d2
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-08 22:28:31 +0100

    changed position ot sticky

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import Navbar from './Navbar'
 
 function Header({navBarOpen, toggleNav}) {
   return (
-    <header className='header'>
+    <header className='header sticky top-0 left-0' style={{
+      width: "100vw"
+    }}>
         <Navbar navBarOpen={navBarOpen} toggleNav={toggleNav} />
     </header>
   )
