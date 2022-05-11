c6edf9902b26bc90a1d46f2a4f6a37c13baf2999
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-11 21:31:56 +0100

    made some fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,12 +9,12 @@ function Navbar({navBarOpen, toggleNav}) {
         <>
             <nav className={`relative flex flex-wrap items-center justify-between px-2 xl:px-8  lg:px-10 py-4 ${classes.blueish}`}>
 
-                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                <div className="container px-4 m-auto flex flex-wrap items-center justify-between">
 
                     {/* Logo and button container */}
                     <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start
                     md:flex-row-reverse mb-3' >
-                        <a href="#pablo" className="text-xl md:text-2xl lg:text-3xl xl:text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white pt-auto">
+                        <a href="#Home" className="text-xl md:text-2xl lg:text-3xl xl:text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white pt-auto">
                             <Logo />
                         </a>
                         <button className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-transparent bg-transparent border-solid rounded block lg:hidden outline-none focus:outline-none" type='button' onClick={() => toggleNav()}>
@@ -27,20 +27,20 @@ function Navbar({navBarOpen, toggleNav}) {
                     <div className={` lg:flex flex-grow items-center ${navBarOpen ?"flex":"hidden "}   `} id="example-navbar-danger">
                         <ul className="flex flex-col lg:flex-row pt-auto list-none lg:ml-auto">
                             <li className="nav-item">
-                                <a href="#logo" className="px-3 py-2 flex items-center text-sm 
-                                md:text-sm lg:text-md xl:text-md pr-6 pb-4 leading-snug text-white hover:opacity-45">
+                                <a href="#share" className="px-3 py-2 flex items-center text-sm 
+                                md:text-sm lg:text-md xl:text-md pr-6 pb-4 leading-snug text-white hover:opacity-35">
                                     <UilFacebook /><span className="ml-2">Share</span>
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a href="#santos" className="px-3 py-2 flex pr-6 pb-4 items-center text-sm 
-                                md:text-sm lg:text-md xl:text-md leading-snug text-white hover:opacity-45">
+                                <a href="#post" className="px-3 py-2 flex pr-6 pb-4 items-center text-sm 
+                                md:text-sm lg:text-md xl:text-md leading-snug text-white hover:opacity-35">
                                     <UilInstagram /><span className="ml-2">Post</span>
                                 </a>
                             </li>
                             <li className="nav-item ">
-                                <a href="#santos" className="px-3 py-2 pr-4 flex items-center text-sm 
-                                md:text-sm lg:text-md xl:text-md leading-snug text-white hover:opacity-45">
+                                <a href="#tweet" className="px-3 py-2 pr-4 flex items-center text-sm 
+                                md:text-sm lg:text-md xl:text-md leading-snug text-white hover:opacity-35">
                                     <UilTwitter /><span className="ml-2">Tweet</span>
                                 </a>
                             </li>
