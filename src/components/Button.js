3d1cd44d0b6284118d09660eb1d531cf00a17eb4
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-12 23:03:27 +0100

    made the button text a little bolder

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ function Button({text, bgColor, textColor}) {
 
   return (
     <div>
-        <input type="button" value={text||"text"} className='text-white cursor-pointer text-xl leading-none px-6 hover:opacity-30 py-4 border rounded-lg border-solid border-1 text-sm block'
+        <input type="button" value={text||"text"} className='text-white text-bold cursor-pointer text-xl leading-none px-6 hover:opacity-30 py-4 border rounded-lg border-solid border-1 text-sm block'
         style={{
           backgroundColor: `${bgColor}`,
           color: `${textColor}`,
