8e1456c4bd948053ff6b78c837e9218901cf3b5f
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-13 18:02:42 +0100

    Created the "Home" page component, pun intended

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Showcase from '../components/Showcase'
+
+function Home() {
+  return (
+    <div>
+        <Showcase />
+    </div>
+  )
+}
+
+export default Home
