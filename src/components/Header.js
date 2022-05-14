ef9c62b271962265295044f79a821a1762edb147
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-15 00:12:08 +0100

    styled the border using tailwindCSS instead of the typical CSS3 class style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,8 @@ import Navbar from './Navbar'
 
 function Header({navBarOpen, toggleNav}) {
   return (
-    <header className='header sticky  top-0 left-0' style={{
-      width: "100vw",
-      borderBottom: "2px solid rgba(255, 255, 255, .25)"
+    <header className='header sticky  top-0 left-0 border-b border-white' style={{
+      width: "100vw"
     }}>
         <Navbar navBarOpen={navBarOpen} toggleNav={toggleNav} />
     </header>
