175a47faa7a6da53c7f277ef135a0da4593449a5
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-14 23:33:52 +0100

    created a HorizontalLine component

---
+++ src/HorizontalLine.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HorizontalLine() {
+  return (
+    <>
+        <span className="lg:hidden w-full border-b my-border-b"></span>
+    </>
+  )
+}
+
+export default HorizontalLine
