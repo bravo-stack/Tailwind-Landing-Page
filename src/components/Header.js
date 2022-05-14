55b82e1412a234ee37e7eb982280f4aa816825e2
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-14 23:42:46 +0100

    made the border bottom a little transparent

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header({navBarOpen, toggleNav}) {
   return (
     <header className='header sticky  top-0 left-0' style={{
       width: "100vw",
-      borderBottom: "1px solid white"
+      borderBottom: "2px solid rgba(255, 255, 255, .25)"
     }}>
         <Navbar navBarOpen={navBarOpen} toggleNav={toggleNav} />
     </header>
