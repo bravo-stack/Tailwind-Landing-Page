c91e966bd412e776002b0d32e8197131f07ec766
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-14 19:09:50 +0100

    The "Home" page component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Showcase from '../components/Showcase'
+import Wrapper from '../components/Wrapper'
+import Features from '../components/Features'
 
 function Home() {
   return (
     <div>
         <Showcase />
+        <Wrapper>
+          <Features />
+        </Wrapper>
     </div>
   )
 }
