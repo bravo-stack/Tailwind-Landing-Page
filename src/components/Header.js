ef034a9aded05e6954e173a80b28cb7b969a7626
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-14 23:34:56 +0100

    refactored the navbar code, made a lotta fixes to the stlying and all, you get me "LAUGHS"

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,9 @@ import Navbar from './Navbar'
 
 function Header({navBarOpen, toggleNav}) {
   return (
-    <header className='header sticky top-0 left-0' style={{
-      width: "100vw"
+    <header className='header sticky  top-0 left-0' style={{
+      width: "100vw",
+      borderBottom: "1px solid white"
     }}>
         <Navbar navBarOpen={navBarOpen} toggleNav={toggleNav} />
     </header>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,51 +2,73 @@ import Logo from './Logo'
 import { UilApps } from '@iconscout/react-unicons'
 import classes from "./navbar.module.css"
 import { Link } from "react-router-dom"
+import HorizontalLine from '../HorizontalLine'
 
 function Navbar({navBarOpen, toggleNav}) {
-    
 
     return (
         <>
-            <nav className={`my-box-shadow relative flex flex-wrap items-center justify-between px-2 xl:px-8  lg:px-10 py-4 ${classes.blueish}`}>
+            <nav className={`my-box-shadow lg:shadow-none relative flex flex-wrap items-center justify-between  ${classes.blueish}`}>
 
-                <div className="container px-4 m-auto flex flex-wrap items-center justify-between">
+                <div className="lg:px-8 lg:py-6 w-full flex flex-wrap items-center justify-between">
 
                     {/* Logo and button container */}
-                    <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start
-                    md:flex-row-reverse mb-3' >
-                        <Link to="/" className="text-xl md:text-2xl lg:text-3xl xl:text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white pt-auto">
+                    <div className='px-8 lg:px-0 py-7 lg:py-0 w-screen relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start
+                    md:flex-row-reverse' >
+                        <Link to="/" className="text-xl md:text-2xl lg:text-3xl font-bold  inline-block  whitespace-nowrap text-white ">
                             <Logo />
                         </Link>
-                        <button className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-transparent bg-transparent border-solid rounded block lg:hidden outline-none focus:outline-none" type='button' onClick={() => toggleNav()}>
+                        <button className="text-white cursor-pointer text-xl leading-none border border-transparent bg-transparent border-solid rounded block lg:hidden outline-none focus:outline-none" type='button' onClick={() => toggleNav()}>
                             <UilApps />
                         </button>
                     </div>
 
 
                     {/* responsive dropdown */}
-                    <div className={` lg:flex flex-grow items-center ${navBarOpen ?"flex":"hidden "}   `} id="example-navbar-danger">
-                        <ul className="flex flex-col lg:flex-row pt-auto list-none lg:ml-auto">
-                            <li className="nav-item">
-                                <Link to="/about" className="px-3 py-2 flex items-center text-sm 
-                                md:text-sm lg:text-md xl:text-md pr-6 pb-4 leading-snug text-white my-link">
-                                    <span className="ml-2 uppercase">About</span>
+                    <div className={`
+                    lg:flex 
+                    flex-grow 
+                    bg-white 
+                    py-5 lg:py-0
+                    lg:bg-opacity-0 justify-center lg:justify-end ${navBarOpen ?"flex":"hidden "}`}>
+                        <ul className="
+                        flex flex-col lg:flex-row
+                        gap-5
+                        list-none lg:ml-auto
+                        w-full lg:w-auto">
+                            <li className="nav-item 
+                            m-auto ">
+                                <Link to="/about" className=" flex 
+                                items-center text-sm
+                                md:text-sm lg:text-md xl:text-md  leading-snug
+                                lg:text-white
+                                my-text-primary
+                                 my-link">
+                                    <span className="uppercase">About</span>
                                 </Link>
                             </li>
-                            <li className="nav-item">
-                                <Link to="/services" className="px-3 py-2 flex pr-6 pb-4 items-center text-sm 
-                                md:text-sm lg:text-md xl:text-md leading-snug text-white my-link">
-                                    <span className="ml-2 uppercase">Services</span>
+                            <HorizontalLine />
+                            <li className="nav-item
+                            m-auto">
+                                <Link to="/services" className=" flex  items-center text-sm 
+                                md:text-sm lg:text-md 
+                                lg:text-white
+                                xl:text-md leading-snug my-text-primary my-link">
+                                    <span className="uppercase">Services</span>
                                 </Link>
                             </li>
-                            <li className="nav-item ">
-                                <Link to="contact" className="px-3 py-2 pr-4 flex items-center text-sm 
-                                md:text-sm lg:text-md xl:text-md leading-snug text-white my-link">
-                                    <span className="ml-2 uppercase">Contact Us</span>
+                            <HorizontalLine />
+                            <li className="nav-item 
+                            m-auto">
+                                <Link to="contact" className=" flex items-center text-sm 
+                                md:text-sm lg:text-md xl:text-md 
+                                lg:text-white
+                                leading-snug my-text-primary my-link">
+                                    <span className="uppercase ">Contact Us</span>
                                 </Link>
                             </li>
+                            
                         </ul>
-                    
                     </div>
                 </div>
             </nav>
