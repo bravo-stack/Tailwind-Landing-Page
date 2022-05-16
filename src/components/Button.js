02eb2cb5c34104d904a9101d381df365807d0b10
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-16 13:05:22 +0100

    reduced the padding across the x-axis for smaller devices "smartphones"

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ function Button({text, bgColor, textColor}) {
 
   return (
     <div>
-        <input type="button" value={text||"text"} className='text-white text-bold cursor-pointer text-xl leading-none px-6 py-4 border rounded-lg border-solid border-1 text-sm block hover:scale-110'
+        <input type="button" value={text||"text"} className='text-white text-bold cursor-pointer text-xl leading-none px-3 py-4 md:px-6 lg:px-6 border rounded-lg border-solid border-1 text-sm block hover:scale-110'
         style={{
           backgroundColor: `${bgColor}`,
           color: `${textColor}`,
