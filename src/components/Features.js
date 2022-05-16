55c5b1e9a42346772ebfd4d50b05b68bc15f7cbf
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-16 16:50:11 +0100

    Feature component init

---
+++ src/components/Features.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Card from './Card'
+import multitasking from "./../assets/multitasking.png"
+import infographics from "./../assets/Infographics.png"
+
+function Features() {
+
+  const data = {
+    firstText: "Accessable features across the globe made easier with just a few click, no more no less. Tasks get assigned efficiently accross all teams; promoting remote workspace",
+    secondText: "Get essential informations mapped out and ready for user consumption. Populating charts for detailed information; with the Infographic section, one can also stay miles ahead"
+  }
+
+
+
+
+  return (
+    <div className='py-4 lg:py-6 flex flex-col gap-4 lg:gap-6'>
+        <div className="text-center text-3xl font-bold">
+            Features
+        </div>
+        <div className="flex flex-col md:grid md:grid-cols-2 md:gap-2 md:gap-x-4 lg:grid lg:grid-cols-4 lg:gap-4">
+          <Card text={data.firstText} img={multitasking} />
+          <Card text={data.secondText} img={infographics} />
+          <Card text={data.firstText} img={multitasking} />
+          <Card text={data.secondText} img={infographics} />
+          <Card text={data.firstText} img={multitasking} />
+          <Card text={data.secondText} img={infographics} />
+          <Card text={data.firstText} img={multitasking} />
+          <Card text={data.secondText} img={infographics} />
+        </div>
+    </div>
+  )
+}
+
+export default Features
