ad200c34808a33f9881fd76ae7ecbc15320669ab
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-16 05:15:26 +0100

    reduced the padding for smaller devices

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Wrapper({children}) {
   return (
     <div>
-        <section className='container p-10'>
+        <section className='container p-6 md:p-10 lg:p-10'>
             {children}
         </section>
     </div>
