d547fe72d4605e3d03ed547b1245d3f2e68dff58
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-16 14:39:30 +0100

    made this component more responsive, introduced a new div

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -4,7 +4,9 @@ function Wrapper({children}) {
   return (
     <div>
         <section className='container p-5 md:p-10 lg:p-10'>
-            {children}
+            <div className='lg:p-6 md:p-6 p-2 pt-6 pb-3'>
+              {children}
+            </div>
         </section>
     </div>
   )
