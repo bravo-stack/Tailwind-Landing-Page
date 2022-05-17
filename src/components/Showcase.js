37f4da707177b31b46b2e2c6be3c558de7bc8ac2
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 08:24:51 +0100

    added padding top to make it look appealing

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -8,9 +8,13 @@ function Showcase() {
     <div className={`rounded-b-3xl border-none my-bg-blue`}>
       <Wrapper>
          <div className='
-         py-12 
-         lg:py-12 
-         xl:py-16'>
+          pb-16
+          pt-8
+          md:pt-12
+          md:pb-24
+          lg:pt-16
+          lg:pb-16
+          xl:pb-20'>
             <div className=' 
             leading-tight
             md:leading-snug 
@@ -19,9 +23,8 @@ function Showcase() {
             text-center 
             text-white 
             flex 
-            flex-col 
-            gap-11 
-            md:gap-16 
+            flex-col-reverse
+            md:gap-4
             lg:gap-8 
             lg:flex-row lg:content-between 
             justify-items-start ' 
@@ -29,41 +32,41 @@ function Showcase() {
               width: "auto",
               height: "max-content",
             }}>
-            <div className='
-            flex 
-            flex-col 
-            gap-5 
-            xl:gap-10 
-            lg:gap-8'>
-              <div className='leading-normal xl:leading-normal lg:leading-normal md:leading-normal text-2xl font-bold xl:text-5xl lg:text-3xl md:text-3xl'>
-                Exclusive Agency For <br className='lg:hidden' />Technology, Providing Solutions
+              <div className='
+              flex 
+              flex-col 
+              gap-5 
+              xl:gap-10 
+              lg:gap-8'>
+                <div className='leading-normal xl:leading-normal lg:leading-normal md:leading-normal text-2xl font-bold xl:text-5xl lg:text-3xl md:text-3xl'>
+                  Exclusive Agency For <br className='lg:hidden' />Technology, Providing Solutions
+                </div>
+                <div className='text-sm xl:text-lg md:text-base text-gray-300 font-light xl:my-0 lg:my-2 md:my-5 my-4'>
+                  We are Mont, a Creative Design Wheel
+                  <span className='my-4 block xl:my-7'></span>
+                  <span className='lg:leading-normal xl:leading-loose'>Helping you maximize operations management with digitization</span>
+                </div>
+                <div className=" flex md:flex-row lg:flex-row gap-4 content-between m-auto lg:m-0 md:gap-6 lg:gap-7">
+                  <Button 
+                  text="Key Features"
+                  bgColor="white"
+                  textColor="rgb(48,24,186)" />
+                  <Button 
+                  text="How We Work?"
+                  bgColor="transparent"
+                  textColor="white" />
+                </div>
               </div>
-              <div className='text-sm xl:text-lg md:text-base text-gray-300 font-light xl:my-0 lg:my-2 md:my-5 my-4'>
-                We are Mont, a Creative Design Wheel
-                <span className='my-4 block xl:my-7'></span>
-                <span className='lg:leading-normal xl:leading-loose'>Helping you maximize operations management with digitization</span>
+              <div style={{
+                paddingBottom: "1rem",
+              }}>
+                <img className='m-auto' src={showcaseImg} alt="" style={{
+                  width: "100%",
+                  height: "auto"
+                }} />
               </div>
-              <div className=" flex md:flex-row lg:flex-row gap-4 content-between m-auto lg:m-0 md:gap-6 lg:gap-7">
-                <Button 
-                text="Key Features"
-                bgColor="white"
-                textColor="rgb(48,24,186)" />
-                <Button 
-                text="How We Work?"
-                bgColor="transparent"
-                textColor="white" />
-              </div>
-            </div>
-            <div style={{
-              paddingBottom: "1rem",
-            }}>
-              <img className='m-auto' src={showcaseImg} alt="" style={{
-                width: "100%",
-                height: "auto"
-              }} />
             </div>
           </div>
-        </div>
       </Wrapper>
     </div>
   )
