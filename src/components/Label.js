8eda1a06c88853f58edf37db82e8860e2ed530ff
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 06:18:24 +0100

    created the label component to seperate sections of the page

---
+++ src/components/Label.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Label({text}) {
+  return (
+    <div>
+        <div className="text-center text-xl md:text-2xl lg:text-3xl font-bold flex text-white">
+            <p className='mx-auto my-bg-blue py-2 px-4 rounded-3xl shadow-xl'>{text}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Label
