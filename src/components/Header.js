20d9b40c149db72341fff9faea2c50523b8f83a5
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 12:20:58 +0100

    change the width from "100vw" to "100%", which fixed the bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 function Header({navBarOpen, toggleNav}) {
   return (
     <header className='header sticky  top-0 left-0 z-10' style={{
-      width: "100vw"
+      width: "100%"
     }}>
         <Navbar navBarOpen={navBarOpen} toggleNav={toggleNav} />
     </header>
