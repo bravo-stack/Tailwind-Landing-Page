c4f3a3711447cbde4b90859999b92255e21f371f
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 10:23:16 +0100

    made fixes to the component's layout

--- src/components/CardText.js
+++ src/components/CardText.js
@@ -7,17 +7,17 @@ function CardText({text}) {
         <div className={` 
         flex flex-col justify-items-center 
         p-6 lg:p-6 text-left text-sm md:text-lg`}>
-            <p className='font-bold text-base md:text-lg'>
+            <p className='font-bold text-lg md:text-lg lg:text-xl pb-1'>
                 Illustration
             </p>
-            <p className='text-sm text-gray-400'>
+            <p className='text-sm text-gray-400 mb-6'>
                 Assign Tasks more efficiently
             </p>
             <p className='
             my-card-text-transition 
             cursor-pointer
-            flex content-center justify-items-center my-text-primary text-lg font-semi-bold pt-6'>
-                <span className='my-auto mr-2'>Get on it</span>
+            flex content-center justify-items-center my-text-primary text-lg font-semi-bold'>
+                <span className='my-auto mr-2'>View More</span>
                 <div className='card-icon inline-block flex content-center justify-items-center my-1 my-transition'>
                     <UilArrowRight />
                 </div>
