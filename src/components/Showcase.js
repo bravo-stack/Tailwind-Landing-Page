1dd44f55e1d09b06658b93a29d5f42a5bf58ca95
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 08:03:21 +0100

    refactored it to be more responsive

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -7,7 +7,10 @@ function Showcase() {
   return (
     <div className={`rounded-b-3xl border-none my-bg-blue`}>
       <Wrapper>
-         <div>
+         <div className='
+         py-12 
+         lg:py-12 
+         xl:py-16'>
             <div className=' 
             leading-tight
             md:leading-snug 
@@ -15,22 +18,30 @@ function Showcase() {
             md:text-center 
             text-center 
             text-white 
-            flex flex-col 
-            gap-11 md:gap-16 lg:gap-0 
+            flex 
+            flex-col 
+            gap-11 
+            md:gap-16 
+            lg:gap-8 
             lg:flex-row lg:content-between 
             justify-items-start ' 
             style={{
               width: "auto",
-              minHeight: "45vh",
+              height: "max-content",
             }}>
-            <div className='flex flex-col gap-5 xl:gap-14 lg:gap-8'>
+            <div className='
+            flex 
+            flex-col 
+            gap-5 
+            xl:gap-10 
+            lg:gap-8'>
               <div className='leading-normal xl:leading-normal lg:leading-normal md:leading-normal text-2xl font-bold xl:text-5xl lg:text-3xl md:text-3xl'>
                 Exclusive Agency For <br className='lg:hidden' />Technology, Providing Solutions
               </div>
-              <div className='text-sm xl:text-xl md:text-base text-gray-300 font-light xl:my-0 lg:my-2 md:my-5 my-4'>
+              <div className='text-sm xl:text-lg md:text-base text-gray-300 font-light xl:my-0 lg:my-2 md:my-5 my-4'>
                 We are Mont, a Creative Design Wheel
                 <span className='my-4 block xl:my-7'></span>
-                Helping you maximize operations management with digitization
+                <span className='lg:leading-normal xl:leading-loose'>Helping you maximize operations management with digitization</span>
               </div>
               <div className=" flex md:flex-row lg:flex-row gap-4 content-between m-auto lg:m-0 md:gap-6 lg:gap-7">
                 <Button 
