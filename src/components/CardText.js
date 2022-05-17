cad295c6d90f40e14cc6809b685e4b5179193ff4
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 09:01:26 +0100

    styled the individual text components that will hold the essential text in this component

--- src/components/CardText.js
+++ src/components/CardText.js
@@ -7,14 +7,20 @@ function CardText({text}) {
         <div className={` 
         flex flex-col justify-items-center 
         p-6 lg:p-6 text-left text-sm md:text-lg`}>
-            <p>
+            <p className='font-bold text-base md:text-lg'>
                 Illustration
             </p>
-            <p>
+            <p className='text-sm text-gray-400'>
                 Assign Tasks more efficiently
             </p>
-            <p>
-                Get on it <UilArrowRight />
+            <p className='
+            my-card-text-transition 
+            cursor-pointer
+            flex content-center justify-items-center my-text-primary text-lg font-semi-bold pt-6'>
+                <span className='my-auto mr-2'>Get on it</span>
+                <div className='card-icon inline-block flex content-center justify-items-center my-1 my-transition'>
+                    <UilArrowRight />
+                </div>
             </p>
         </div>
     </div>
