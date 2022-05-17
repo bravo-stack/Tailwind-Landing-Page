51adb182a2b0bbd0b0d5ab1edc77a47bf1659bc3
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 13:49:16 +0100

    made some fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
-import React from 'react'
 import Navbar from './Navbar'
 
 function Header({navBarOpen, toggleNav}) {
   return (
-    <header className='header sticky  top-0 left-0 z-10' style={{
+    <header className='header sticky  top-0 left-0 z-10 my-bg-blue' style={{
       width: "100%"
     }}>
-        <Navbar navBarOpen={navBarOpen} toggleNav={toggleNav} />
+      <Navbar navBarOpen={navBarOpen} toggleNav={toggleNav} />
     </header>
   )
 }
