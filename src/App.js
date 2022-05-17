363895c06d42a8df80b84918b5319955b8e57eb5
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 13:59:30 +0100

    Added to the Three "About, Services and Contact Us" pages to the react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import About from "./pages/About";
 
 function App() {
   const [navBarOpen, setNavBarOpen] = useState(false)
@@ -20,6 +21,9 @@ function App() {
         
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<About />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </Router>
