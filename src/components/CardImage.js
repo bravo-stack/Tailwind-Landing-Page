a2b86e7de8b7af1a80d4e0055ab164e5616175d0
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 08:59:52 +0100

    removed the bottom border radius which was previously visible from larger "xl" devices

--- src/components/CardImage.js
+++ src/components/CardImage.js
@@ -6,7 +6,6 @@ function CardImage({img}) {
         <div className={` 
             my-bg-blue-lite 
             w-full 
-            lg:rounded
             lg:rounded-t-2xl
             rounded-t-2xl`}>
                 <img className='my-transition hover:scale-110'  src={img} alt="text" />
