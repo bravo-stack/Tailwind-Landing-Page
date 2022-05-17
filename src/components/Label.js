67dd5b9eaf7e43c350e54c2d1b42dc447bf97ebd
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 11:55:48 +0100

    changed the appearance of the label component

--- src/components/Label.js
+++ src/components/Label.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Label({text}) {
   return (
     <div>
-        <div className="text-center text-xl md:text-2xl lg:text-2xl xl:text-3xl font-bold flex text-white">
-            <p className='mx-auto my-bg-blue py-2 px-4 rounded-3xl shadow-xl'>{text}</p>
+        <div className="text-center text-xl md:text-2xl lg:text-2xl xl:text-3xl font-bold flex my-text-primary">
+            <p className='mx-auto label-bg py-2 px-4 rounded-3xl shadow-xl'>{text}</p>
         </div>
     </div>
   )
