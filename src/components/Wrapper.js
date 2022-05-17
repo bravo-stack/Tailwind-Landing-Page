af5b37bab31498cb8593dc2cb9149f4129d2d332
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 12:08:05 +0100

    fixed some issues by removing the "container" class to make it more responsive

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Wrapper({children}) {
   return (
     <div>
-        <section className='container px-5 md:px-10 lg:px-12 xl:px-16'>
+        <section className='px-5 md:px-10 lg:px-12 xl:px-16'>
             <div className='lg:px-6 md:px-6 px-2'>
               {children}
             </div>
