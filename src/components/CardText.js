7b579305b9a1fe961f724aff4f651646fcab7800
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 10:52:04 +0100

    added some cute transitions to the view more text, and the icon beside it

--- src/components/CardText.js
+++ src/components/CardText.js
@@ -7,21 +7,21 @@ function CardText({text}) {
         <div className={` 
         flex flex-col justify-items-center 
         p-6 lg:p-6 text-left text-sm md:text-lg`}>
-            <p className='font-bold text-lg md:text-lg lg:text-xl pb-1'>
+            <p className='font-bold text-lg md:text-lg lg:text-xl pb-1 card-header'>
                 Illustration
             </p>
             <p className='text-sm text-gray-400 mb-6'>
                 Assign Tasks more efficiently
             </p>
-            <p className='
-            my-card-text-transition 
+            <div className='
+            my-view-more
             cursor-pointer
             flex content-center justify-items-center my-text-primary text-lg font-semi-bold'>
-                <span className='my-auto mr-2'>View More</span>
+                <span className='my-auto mr-2 my-transition'>View More</span>
                 <div className='card-icon inline-block flex content-center justify-items-center my-1 my-transition'>
                     <UilArrowRight />
                 </div>
-            </p>
+            </div>
         </div>
     </div>
   )
