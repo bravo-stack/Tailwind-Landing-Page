01e2d1d4faf14de6367553c16fe6b2fcaffcd854
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-17 06:20:05 +0100

    introduced the lable component

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from './Card'
 import multitasking from "./../assets/multitasking.png"
 import infographics from "./../assets/Infographics.png"
+import Label from './Label'
 
 function Features() {
 
@@ -10,15 +11,25 @@ function Features() {
     secondText: "Get essential informations mapped out and ready for user consumption. Populating charts for detailed information; with the Infographic section, one can also stay miles ahead"
   }
 
-
-
-
   return (
-    <div className='py-4 lg:py-6 flex flex-col gap-4 lg:gap-6'>
-        <div className="text-center text-3xl font-bold">
-            Features
-        </div>
-        <div className="flex flex-col md:grid md:grid-cols-2 md:gap-2 md:gap-x-4 lg:grid lg:grid-cols-4 lg:gap-4">
+    <div className='
+    py-4 
+    lg:py-6 
+    flex 
+    flex-col 
+    gap-4 
+    lg:gap-6'>
+        <Label text="Features" />
+        <div className="
+        flex 
+        flex-col 
+        md:grid 
+        md:grid-cols-2 
+        md:gap-2 
+        md:gap-x-4 
+        lg:grid 
+        lg:grid-cols-4 
+        lg:gap-4">
           <Card text={data.firstText} img={multitasking} />
           <Card text={data.secondText} img={infographics} />
           <Card text={data.firstText} img={multitasking} />
