97a848a9dc52bd3a18eca98ea72c5a2655b6c6fa
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-18 19:36:15 +0100

    fixed some texts

--- src/components/CardText.js
+++ src/components/CardText.js
@@ -9,7 +9,7 @@ function CardText({text}) {
         flex flex-col justify-items-center 
         p-6 lg:p-6 text-left text-sm md:text-lg`}>
             <p className='my-transition font-bold text-lg md:text-lg lg:text-xl pb-1 card-header'>
-                Illustration
+                Distributed Teams
             </p>
             <p className='text-sm text-gray-400 mb-6'>
                 Assign Tasks more efficiently
@@ -19,7 +19,7 @@ function CardText({text}) {
                 my-view-more
                 cursor-pointer
                 flex content-center justify-items-center my-text-primary text-lg font-semi-bold'>
-                    <span className='my-auto mr-2 my-transition'>View All</span>
+                    <span className='my-auto mr-2 my-transition'>Learn More</span>
                     <div className='card-icon inline-block flex content-center justify-items-center my-1 my-transition'>
                         <UilArrowRight />
                     </div>
