8ce73f96efc4b151d5601a6f1832b515f493fadb
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-19 20:26:29 +0100

    GettingStarted component init

---
+++ src/GetStarted.js
@@ -0,0 +1,22 @@
+import working from "./assets/content-man-image.png"
+
+function GetStarted() {
+  return (
+    <div>
+        <section className='my-bg-sec'>
+            <div className="pt-12">
+                <div className="flex ">
+                    <img className="md:w-1/2 lg:w-1/3" src={working} alt="get started" />
+                </div>
+            </div>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default GetStarted
