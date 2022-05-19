acb9d8b71bc1b31d61bd65b1e38a7f5f5923dc0c
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-19 22:22:18 +0100

    completed the GettingStarted component

---
+++ src/GetAccessCard.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function GetAccessCard() {
+    return (
+        <div className=' getstarted-rounded p-10 shadow-xl bg-white flex flex-col gap-5 lg:gap-10 leading-normal my-auto'>
+            <div className='flex flex-col gap-6 lg:gap-8 '>
+                <p className="text-3xl lg:text-5xl font-bold my-text-primary">
+                    Get access to your daily cost.
+                </p>
+                <p className='text-gray-600 lg:leading-loose'>
+                    With lots of unique blocks, you can easily build a page without coding. Build your next website within few minutes.
+                </p>
+                <div>
+                    <input type="button" value="Get started now" className='
+                    shadow-xl 
+                    lg:w-auto 
+                    text-white 
+                    text-bold 
+                    cursor-pointer 
+                    text-xl 
+                    lg:text-xl 
+                    leading-none 
+                    px-4 
+                    py-5 
+                    lg:px-8 
+                    md:px-6 
+                    lg:px-6 
+                    border 
+                    rounded-lg l
+                    g:rounded-2xl 
+                    border-solid 
+                    border-1 
+                    text-sm 
+                    block 
+                    hover:scale-110'
+                    style={{
+                    backgroundColor: `rgb(48, 24, 186)`,
+                    color: `white`,
+                    transition: "all .4s ease-in-out"
+                    }} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GetAccessCard
--- src/GetStarted.js
+++ src/GetStarted.js
@@ -1,19 +1,29 @@
 import working from "./assets/content-man-image.png"
+import Wrapper from "./components/Wrapper"
+import GetAccessCard from "./GetAccessCard"
 
 function GetStarted() {
   return (
     <div>
-        <section className='my-bg-sec'>
-            <div className="pt-12">
-                <div className="flex ">
-                    <img className="md:w-1/2 lg:w-1/3" src={working} alt="get started" />
+        <section className='my-bg-sec pt-10 lg:pt-0'>
+            <Wrapper>
+
+                {/* Getting started container */}
+                <div className="flex flex-col lg:flex-row-reverse">
+
+                    {/* Getting Started card view */}
+                    <div className="flex lg:w-1/2 md:w-2/3 m-auto">
+                        <GetAccessCard />
+                    </div>
+
+                    {/* Getting started image */}
+                    <div className="pt-12 lg:w-1/2 md:w-2/3 m-auto">
+                        <div className="flex ">
+                            <img className="" src={working} alt="get started" />
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div>
-                    
-                </div>
-            </div>
+            </Wrapper>
         </section>
     </div>
   )
