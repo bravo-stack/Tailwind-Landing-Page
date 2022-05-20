400694b2bf9ee72284c3e1860456ccc0bb96d312
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-20 14:50:53 +0100

    testimonialcard component init

---
+++ src/components/TestimonialCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function TestimonialCard() {
+  return (
+    <div>
+        <div className='shadow'>
+            <div>
+                <img src="" alt="team member" />
+            </div>
+            <div>
+                <p>
+                    “You made it so simple. My new site is so much faster and easier to work with than my old site.”
+                </p>
+                <p>
+                    Alan Farmer
+                </p>
+                <p>
+                    Digital Marketer
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TestimonialCard
