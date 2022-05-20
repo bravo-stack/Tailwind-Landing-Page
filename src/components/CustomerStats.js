13940d4b9bfe4726399647995fabf37e36237f7a
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-20 14:06:41 +0100

    completed the Stats comonent

--- src/components/CustomerStats.js
+++ src/components/CustomerStats.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import GrayLine from './GrayLine'
 import Stats from './Stats'
+import Wrapper from './Wrapper'
 
 function CustomerStats() {
   return (
     <div>
+      <Wrapper>
         <Stats />
+      </Wrapper>
+      <GrayLine />
     </div>
   )
 }
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,37 +6,51 @@ function Stats() {
   return (
     <div>
         <section>
+
+            {/* label container */}
             <div className='mt-16'>
-                <Label text="Stats" />
+                <Label text="Users Stats" />
             </div>
-            <div className='mb-9 mt-9 text-center'>
-                <p className='my-text-primary text-3xl font-bold mb-5'>
-                    Trusted by 35,000+ happy customers.
-                </p>
-                <p className='text-gray-600'>
-                    With lots of unique blocks, you can easily build a page without coding.
-                    Build your next website within few minutes.
-                </p>
-            </div>
-            <div>
-                <div className='p-10 statcard-size rounded-3xl my-bg-blue2 m-2'>
-                    <p className='text-6xl font-extrabold text-white'>
-                        21k
+
+            {/* entire rating container */}
+            <div className='flex flex-col xl:flex-row mb-16 mt-9'>
+
+                {/* "trusted by customer" container */}
+                <div className='xl:w-1/3 lg:my-auto'>
+                    <p className='my-text-primary text-center xl:text-left text-4xl font-medium md:font-semibold lg:font-bold mb-6'>
+                        Trusted by 35,000+ happy customers.
                     </p>
-                    <p className='leading-relaxed mt-9 text-lg text-white'>
-                        Active users visiting <br/>us every month!
+                    <p className='text-gray-600 text-center lg:text-justify xl:text-justify'>
+                        With lots of unique blocks, you can easily ship that product, and track it's perfomance, efficiently.
+                        Build your next website within few minutes.
                     </p>
                 </div>
-                <div className='p-10 statcard-size rounded-3xl my-bg-blue3 m-2'>
-                    <p className='text-6xl font-extrabold text-white'>
-                        4.8
-                    </p>
-                    <div className=' mt-9'>
-                        <Rating />
+
+                {/* StatCard container */}
+                <div className='flex flex-col gap-5 mt-12 xl:mt-0 md:flex-row lg:flex-row xl:ml-auto'>
+
+                    {/* Stat card1 */}
+                    <div className='p-10 shadow-xl statcard-size rounded-3xl my-bg-blue2 lg:w-full w-full xl:w-auto xl:w-80 mx-auto'>
+                        <p className='text-6xl font-extrabold text-white'>
+                            21k
+                        </p>
+                        <p className='leading-relaxed mt-9 text-lg text-white'>
+                            Active users visiting <br/>us every month!
+                        </p>
                     </div>
-                    <p className='leading-relaxed mt-3 text-lg text-white'>
-                        2,378 Rating
-                    </p>
+
+                    {/* Statcard 2 */}
+                    <div className='p-10 shadow-xl statcard-size rounded-3xl my-bg-blue3 lg:w-full w-full xl:w-auto xl:w-80 mx-auto'>
+                        <p className='text-6xl font-extrabold text-white'>
+                            4.8
+                        </p>
+                        <div className=' mt-9'>
+                            <Rating />
+                        </div>
+                        <p className='leading-relaxed mt-3 text-lg text-white'>
+                            2,378 Rating
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
