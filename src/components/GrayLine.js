b8d99e03caa1c24b6ba740de56ad99d0e8c022a9
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-20 14:06:09 +0100

    created a grayline component that will seperate component layout on the page

---
+++ src/components/GrayLine.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GrayLine() {
+    return (
+        <>
+            <span className='rounded-lg block h-2 bg-gray-300'></span>
+        </>
+    )
+}
+
+export default GrayLine
