c31da686f92fe3fbefc74814e2f2ee0d202a24b8
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-21 13:53:02 +0100

    completed the Demo component

--- src/DemoForm.js
+++ src/DemoForm.js
@@ -1,19 +1,23 @@
-import React from 'react'
 
 function DemoForm() {
     return (
-        <div className='mt-10'>
-            <form className='flex flex-col gap-6 text-lg'>
-                <label htmlFor="name" className='flex flex-col gap-6'>
+        <div>
+            <form className='flex flex-col gap-8 text-lg'>
+                <label htmlFor="name" className='flex flex-col gap-4'>
                     <span className="font-bold text-white">Your Name</span>
                     <input placeholder="Type your name" className="block w-full rounded-xl p-5" type="text" name="name" id="name" />
                 </label>
                 
-                <label htmlFor="name" className='flex flex-col gap-6'>
+                <label htmlFor="name" className='flex flex-col gap-4'>
                     <span className="font-bold text-white">Email Address</span>
                     <input placeholder="Enter your email" className="block w-full rounded-xl p-5" type="email" name="name" id="name" />
                 </label>
-
+                
+                {/* Button */}
+                <input type="button" value="Submit" className='text-white text-bold cursor-pointer text-xl my-bg-blue border-transparent leading-none p-7 md:px-6 lg:px-6 border rounded-xl border-solid border-1 text-sm block hover:scale-105'
+                style={{
+                transition: "all .4s ease-in-out"
+                }} />
 
             </form>
         </div>
--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,20 +1,45 @@
 import DemoForm from "../DemoForm"
 import Wrapper from "./Wrapper"
+import flower from "./../assets/flower-image.png"
+import rocket from "./../assets/rocket-image.png"
 
 function Demo() {
     return (
-        <div className="my-bg-blue3 pt-16">
+        <div className="my-bg-blue3 pt-16  pb-12 xl:pb-0">
+
+            {/* Wrapper component */}
             <Wrapper>
-                <div className='text-white'>
-                    <p className="text-4xl font-bold text-center mb-8">
-                        Get a Demo
-                    </p>
-                    <p className="text-base text-gray-400 text-center leading-relaxed">
-                        Be one of the first to try out our amazing new features, because those feedbacks would be nice you know.
-                    </p>
-                </div>
-                <div>
-                    <DemoForm />
+
+                {/* Flex container */}
+                <div className="flex">
+
+                    {/* SVG Flower Icon */}
+                    <div className="animate-pulse w-32 relative left-10 bottom-0 xl:inline-flex flex-col-reverse hidden">
+                        <img src={flower} alt="flower img" />
+                    </div>
+
+                    {/* Contact Form Container*/}
+                    <div className="md:w-2/3 lg:w-2/3 xl:w-2/5 md:mx-auto xl:pb-16">
+                        <div className='text-white'>
+                            <p className="text-4xl font-bold text-center mb-8">
+                                Get a Demo
+                            </p>
+                            <p className="text-base text-gray-400 text-center leading-relaxed xl:text-lg">
+                                Be one of the first to try out our amazing new features, because those feedbacks would be nice you know.
+                            </p>
+                        </div>
+
+                        {/* DemoForm Component */}
+                        <div className="mt-10">
+                            <DemoForm />
+                        </div>
+                    </div>
+
+                    {/* SVG Flower Icon */}
+                    <div className="animate-bounce delay-100 w-20 relative right-16 bottom-32 xl:inline-flex flex-col-reverse hidden">
+                        <img src={rocket} alt="flower img" />
+                    </div>
+                    
                 </div>
             </Wrapper>
         </div>
