b54ed80a07d8d85940bdeaefcf0dab6c172d032c
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-22 17:11:25 +0100

    made some fixes

--- src/components/BackDrop.js
+++ src/components/BackDrop.js
@@ -4,6 +4,7 @@ function BackDrop() {
     return (
         <div className='absolute opacity-75 my-h top-0 left-0 w-full bg-black my-z-1000' style={{
             
+            
         }}>
             
         </div>
