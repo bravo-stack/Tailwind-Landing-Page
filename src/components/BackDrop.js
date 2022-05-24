80881f66200e3c28d30afade7c891fdbb0cc8b7a
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-24 07:57:09 +0100

    finally achieved the full width backdrop using the tailwind "h-full" class, and bottom-0 positioning

--- src/components/BackDrop.js
+++ src/components/BackDrop.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function BackDrop() {
     return (
-        <div className='absolute opacity-75 my-h top-0 left-0 w-full bg-black my-z-1000'>
+        <div className='absolute opacity-75 h-full bottom-0 left-0 w-full bg-black my-z-1000'>
 
         </div>
     )
