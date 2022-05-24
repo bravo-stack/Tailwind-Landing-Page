6d02c846294bf5fd0d41689d99031a00b4e48d5d
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-24 18:58:24 +0100

    still debugging

--- src/components/BackDrop.js
+++ src/components/BackDrop.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import SubmitSuccess from './SubmitSuccess'
 
 function BackDrop() {
     return (
-        <div className='absolute opacity-75 h-full bottom-0 left-0 w-full bg-black my-z-1000'>
-
+        <div className='absolute opacity-75 h-full bottom-0 left-0 w-full bg-black my-z-1000 my-transition' style={{
+            border: "5px solid red"
+        }}>
+            <SubmitSuccess />
         </div>
     )
 }
