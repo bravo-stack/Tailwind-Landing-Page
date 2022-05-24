ab84df618deefdf32bc111266148775bb56b3f25
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-24 18:57:55 +0100

    SubmitSuccess component init

--- src/components/SubmitSuccess.js
+++ src/components/SubmitSuccess.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SubmitSuccess() {
     return (
-        <div>
-            
+        <div className='absolute top-0 left-0 my-transition w-2/3 bg-white rounded-2xl shadow-xl m-auto'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas voluptatibus, ipsa ducimus, recusandae, distinctio ex maiores similique repellendus doloribus quibusdam accusamus aut a qui voluptatem repudiandae inventore nihil iusto officia!            
         </div>
     )
 }
