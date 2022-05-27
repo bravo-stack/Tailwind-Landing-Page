4ae20e458b4e654a519465067eb2eb0699926369
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-27 11:41:26 +0100

    done with the backdrop section

--- src/components/BackDrop.js
+++ src/components/BackDrop.js
@@ -4,7 +4,7 @@ import SubmitSuccess from './SubmitSuccess'
 function BackDrop() {
     return (
         <div className='absolute h-full bottom-0 left-0 w-full my-z-1000 my-transition' style={{
-            background: "rgba(0,0,0,.80)"
+            background: "rgba(0,0,0,.90)"
         }}>
             <SubmitSuccess />
         </div>
--- src/components/SubmitSuccess.js
+++ src/components/SubmitSuccess.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {UilCheckCircle} from "@iconscout/react-unicons"
 
 function SubmitSuccess() {
     return (
-        <div className='absolute my-transition w-2/3 bg-white rounded-2xl p-12 shadow-xl m-auto bottom-0'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas voluptatibus, ipsa ducimus, recusandae, distinctio ex maiores similique repellendus doloribus quibusdam accusamus aut a qui voluptatem repudiandae inventore nihil iusto officia!            
+        <div className='fixed top-0 right-0 md:mt-5 lg:mt-7 w-full md:w-auto lg:w-auto flex my-transition bg-white shadow-xl py-3 pt-6 md:pt-3 lg:pt-3 px-3 text-green-500' style={{
+            borderBottom: "6px solid rgb(34 197 94)",
+        }}>
+            <span className='my-auto mx-4 mr-6 md:mx-8 lg:mx-8 '>
+                <UilCheckCircle />
+            </span>
+            <p className='text-lg md:pr-8 lg:pr-8'>
+                <span className='text-2xl lg:text-3xl'>Success!</span><br />We will be in touch with you
+            </p>    
         </div>
     )
 }
