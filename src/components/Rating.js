407ba7c676a763880c371a3100717b9bf48c1a19
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-27 19:47:23 +0100

    Introduced fontawesome to the ecosystem, mehn it is sick! so, i used it in the rating the the submitsuccess components

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,14 +1,16 @@
 import { UilStar } from "@iconscout/react-unicons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function Rating() {
   return (
     <div>
-        <div className="flex text-orange-500">
-            <UilStar />
-            <UilStar />
-            <UilStar />
-            <UilStar />
-            <UilStar />
+        <div className="flex gap-1 text-orange-500">
+            <FontAwesomeIcon icon={faStar} size="lg"/>
+            <FontAwesomeIcon icon={faStar} size="lg"/>
+            <FontAwesomeIcon icon={faStar} size="lg"/>
+            <FontAwesomeIcon icon={faStar} size="lg" />
+            <FontAwesomeIcon icon={faStar} size="lg" />
         </div>
     </div>
   )
--- src/components/SubmitSuccess.js
+++ src/components/SubmitSuccess.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {UilCheckCircle} from "@iconscout/react-unicons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 
 function SubmitSuccess() {
     return (
-        <div className='fixed top-0 right-0 md:mt-5 lg:mt-7 w-full md:w-auto lg:w-auto flex my-transition bg-white shadow-xl py-3 pt-6 md:pt-3 lg:pt-3 px-3 text-green-500' style={{
+        <div className='fixed top-0 right-0 md:mt-5 lg:mt-7 w-full md:w-auto lg:w-auto flex my-transition bg-white shadow-xl py-3 pt-4 md:pt-3 lg:pt-3 px-3 text-green-500' style={{
             borderBottom: "6px solid rgb(34 197 94)",
         }}>
             <span className='my-auto mx-4 mr-6 md:mx-8 lg:mx-8 '>
-                <UilCheckCircle />
+            <FontAwesomeIcon icon={faCircleCheck} size="3x" />
             </span>
             <p className='text-lg md:pr-8 lg:pr-8'>
                 <span className='text-2xl lg:text-3xl'>Success!</span><br />We will be in touch with you
