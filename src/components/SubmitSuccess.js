9139c25ceeae1253bce0377a3627f1fc7eaaa272
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-05-27 10:29:52 +0100

    still trying to align the success message

--- src/components/SubmitSuccess.js
+++ src/components/SubmitSuccess.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SubmitSuccess() {
     return (
-        <div className='absolute top-0 left-0 my-transition w-2/3 bg-white rounded-2xl shadow-xl m-auto'>
+        <div className='absolute my-transition w-2/3 bg-white rounded-2xl p-12 shadow-xl m-auto bottom-0'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas voluptatibus, ipsa ducimus, recusandae, distinctio ex maiores similique repellendus doloribus quibusdam accusamus aut a qui voluptatem repudiandae inventore nihil iusto officia!            
         </div>
     )
