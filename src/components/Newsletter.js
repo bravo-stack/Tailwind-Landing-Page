3ff8d94c71fa181be41f1725d9e37197a44fc9d0
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-06-01 22:45:16 +0100

    wip: continuing the newletter section tomorrow

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -3,25 +3,28 @@ function Newsletter({submit}) {
     
     return (
         // container
-        <div className='bg-white'>
+        <div className='bg-white' style={{width: "300px"}}>
 
             {/* our form */}
             <form className='flex flex-col gap-8 text-lg' onSubmit={submit}>
 
                     {/* first lable which serves as a container here too */}
-                    <label htmlFor="name" className='my-bg-blue-lite flex flex-col gap-4'>
+                    <label htmlFor="name" className='my-bg-blue-lite'>
 
                         {/* label text */}
                         <span className="font-bold">Your Name</span>
 
-                        {/* email input */}
-                        <input placeholder="Type your name" className="w-full rounded-xl inline-block p-5" type="text" name="name" id="name" />
+                        {/* input field and submit btn container */}
+                        <div className="flex p-3 bg-white"  style={{border: "3px solid red"}}>
+                            {/* email input */}
+                            <input placeholder="Type your name" className="w-full rounded-xl p-5" type="text" name="name" id="name" />
 
-                        {/* Our submit button */}
-                        <input type="submit" value="Submit" className='bg-orange-500 text-white text-bold cursor-pointer border-transparent leading-none p-7 border rounded-xl block hover:scale-105'
-                        style={{
-                        transition: "all .4s ease-in-out"
-                        }} />
+                            {/* Our submit button */}
+                            <input type="submit" value="Submit" className='bg-orange-500 text-white text-bold cursor-pointer border-transparent leading-none px-6 border rounded-3xl hover:scale-105'
+                            style={{
+                            transition: "all .4s ease-in-out"
+                            }} />
+                        </div>
                     </label>
                 </form>
         </div>
