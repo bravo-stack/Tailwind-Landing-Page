2ab069062569b05e9cba6243a80e35bb92d07663
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-06-01 21:22:24 +0100

    WIP: newletter component in progress

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,11 +2,26 @@
 function Newsletter({submit}) {
     
     return (
-        <div className='my-bg-blue-lite'>
+        // container
+        <div className='bg-white'>
+
+            {/* our form */}
             <form className='flex flex-col gap-8 text-lg' onSubmit={submit}>
-                    <label htmlFor="name" className='flex flex-col gap-4'>
-                        <span className="font-bold text-white">Your Name</span>
-                        <input placeholder="Type your name" className="block w-full rounded-xl p-5" type="text" name="name" id="name" />
+
+                    {/* first lable which serves as a container here too */}
+                    <label htmlFor="name" className='my-bg-blue-lite flex flex-col gap-4'>
+
+                        {/* label text */}
+                        <span className="font-bold">Your Name</span>
+
+                        {/* email input */}
+                        <input placeholder="Type your name" className="w-full rounded-xl inline-block p-5" type="text" name="name" id="name" />
+
+                        {/* Our submit button */}
+                        <input type="submit" value="Submit" className='bg-orange-500 text-white text-bold cursor-pointer border-transparent leading-none p-7 border rounded-xl block hover:scale-105'
+                        style={{
+                        transition: "all .4s ease-in-out"
+                        }} />
                     </label>
                 </form>
         </div>
