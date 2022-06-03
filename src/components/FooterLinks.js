1d038a0d5656ea5bcb8ba8d195304202185deef4
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-06-03 19:29:57 +0100

    fix: changed the margin-right to auto

--- src/components/FooterLinks.js
+++ src/components/FooterLinks.js
@@ -7,7 +7,7 @@ function FooterLinks() {
 
     return(
         // Footer link containing tag
-        <div className="flex gap-28 md:gap-64 lg:gap-32 px-4 lg:px-auto xl:mr-auto">
+        <div className="flex gap-28 md:gap-64 lg:gap-32 px-4 lg:px-auto lg:mr-auto">
             <CompanyLinks />
             <ProductLinks />
             <ServiceLinks />
