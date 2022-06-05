427d2122a31461df3edba5675746cfa9ff243b39
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-06-06 00:16:39 +0100

    structuring the card component, used the list-key stlye

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
-import React from 'react'
 import CardImage from './CardImage'
 import CardText from './CardText'
 
-function Card({text, img}) {
+function Card({name, title, image}) {
+  
   return (
     <div>
-        <section className={`flex mx-2 my-4  flex-col shadow-xl lg:shadow-lg rounded-2xl`}>
-            <CardImage img={img} />
-            <CardText />
-        </section>
+        <li className={`flex mx-2 my-4  flex-col shadow-xl lg:shadow-lg rounded-2xl`}>
+            <CardImage img={image} />
+            <CardText name={name} title={title} />
+        </li>
     </div>
   )
 }
