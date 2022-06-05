9a21b551039d5343afee352b627c71a22f36e5e7
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-06-06 00:35:23 +0100

    remove the image container's background

--- src/components/CardImage.js
+++ src/components/CardImage.js
@@ -4,7 +4,6 @@ function CardImage({img}) {
   return (
     <div>
         <div className={` 
-            my-bg-blue-lite 
             w-full lg:rounded-t-2xl
             rounded-t-2xl`}>
                 <img className='my-transition hover:scale-110 lg:rounded-t-2xl
