72bef0849056a14c0e6571466ebc91747b71145a
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-06-06 02:08:59 +0100

    Refactored the Testimonial

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,11 +1,47 @@
-import teamimage from "./../assets/team-image-1.png"
-import teamimage2 from "./../assets/team-image-2.png"
-import teamimage3 from "./../assets/team-image-3.png"
 import Label from "./Label"
 import TestimonialCard from "./TestimonialCard"
 import Wrapper from "./Wrapper"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 function Testimonial() {
+    const [testimonial, setTestimonial] = useState([])
+
+    // specifying the base URL
+    const baseURL = "json/testimonial.json"
+
+    // use effect to set data everytime the component mounts
+    useEffect(()=>{
+
+        // axios.get to fetch
+        axios.get(baseURL).then(response=>{
+            
+            // converting object ot arr
+            const temp = response.data
+            const arr = []
+            for(const key in temp.testimonial){
+                const obj = {
+                    id: key,
+                    ...temp.testimonial[key]
+                }
+                
+                arr.push(obj)
+            }
+
+            // Storing th converted arr as our state
+            setTestimonial(arr)
+            console.log(arr)
+         
+        // catching error
+        }).catch(error=>{
+
+            // print error message
+            console.log(error)
+        })
+    }, [])
+
+
+
   return (
     <div>
         <div className="my-bg-sec pt-16 pb-12">
@@ -16,11 +52,9 @@ function Testimonial() {
                 <p className="text-3xl lg:text-4xl my-text-primary text-center font-bold mb-6 lg:mb-10 ">
                     Lots of positive feebacks!
                 </p>
-                <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 lg:gap-7 md:gap-7'>
-                    <TestimonialCard img={teamimage} />
-                    <TestimonialCard img={teamimage2} />
-                    <TestimonialCard img={teamimage3} /> 
-                </div>
+                <ul className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 xl:gap-7 md:gap-7'>
+                    {testimonial&&testimonial.map((data)=>(<TestimonialCard image={data.image} message={data.message} name={data.name} position={data.position} key={data.id} />))}
+                </ul>
             </Wrapper>
         </div>
     </div>
